404e930de44d9eb2c26a7cb531a73bbf6006f190
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-11-25 10:35:14 +0530

    changes in code

--- Main.java
+++ Main.java
@@ -100,37 +100,39 @@
 //
 //     }
 //};
- import java.util.Scanner;
+// import java.util.Scanner;
+//
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//
+//         String adjective1;
+//         String noun1;
+//         String adjective2;
+//         String verb1;
+//         String adjective3;
+//
+//         System.out.print("enter an adjective: ");
+//         adjective1 = scanner.nextLine();
+//
+//         System.out.print("enter a adjective: ");
+//         adjective2 = scanner.nextLine();
+//
+//         System.out.print("enter an  adjective: ");
+//         adjective3 = scanner.nextLine();
+//
+//         System.out.print("enter a noun: ");
+//         noun1 = scanner.nextLine();
+//
+//         System.out.print("enter a verb with -ing (action): ");
+//         verb1 = scanner.nextLine();
+//
+//
+//         System.out.println("\n today i went a " + adjective1 + " zoo");
+//         System.out.println("in an exhibit , i saw a " + noun1 + ".");
+//         System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+//         System.out.println("I was " + adjective3 + "!");
+//     }
+// };
 
- public class Main {
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
 
-         String adjective1;
-         String noun1;
-         String adjective2;
-         String verb1;
-         String adjective3;
-
-         System.out.print("enter an adjective ");
-         adjective1 = scanner.nextLine();
-
-         System.out.print("enter a adjective ");
-         adjective2 = scanner.nextLine();
-
-         System.out.print("enter an  adjective ");
-         adjective3 = scanner.nextLine();
-
-         System.out.print("enter a noun");;
-         noun1 = scanner.nextLine();
-
-         System.out.print("enter a verb with -ing (action)'");
-         verb1 = scanner.nextLine();
-
-
-         System.out.println("today i went a " + adjective1 + " zoo");
-         System.out.println("in an exhibit , i saw a " + noun1 + ".");
-         System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-         System.out.println("I was " + adjective3 + "!");
-     }
- }
