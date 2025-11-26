9f4c48298f0ac1dfe87ab73fba21212e5c3c3a30
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-11-26 18:36:21 +0530

    written code  for random numbers

--- Main.java
+++ Main.java
@@ -166,3 +166,17 @@
 // }
 
 
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+     Scanner scanner = new  Scanner(System.in);
+
+     Random random = new Random();
+
+     int number ;
+     number = random.nextInt(1 ,9);
+        System.out.println(number);
+    };
+};
