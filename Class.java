96b4dd73f6d68fef5ed48cae3924b16dd7710ab9
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-02 18:34:51 +0530

    chanegs

--- Class.java
+++ Class.java
@@ -1,33 +1,33 @@
- import java.util.Random;
- import java.util.Scanner;
-public class Class {
-     public static void main(String[] args){
-         Random random = new Random();
-         Scanner scanner = new Scanner(System.in);
+//  import java.util.Random;
+//  import java.util.Scanner;
+// public class Class {
+//      public static void main(String[] args){
+//          Random random = new Random();
+//          Scanner scanner = new Scanner(System.in);
 
-         int guess;
-         int attempts = 0;
-         int randomNum = random.nextInt(1 ,11);
-         System.out.println("NUmber guessing game");
-         System.out.println("Guess the number between 1-10: ");
+//          int guess;
+//          int attempts = 0;
+//          int randomNum = random.nextInt(1 ,11);
+//          System.out.println("NUmber guessing game");
+//          System.out.println("Guess the number between 1-10: ");
 
-         do{
+//          do{
              
-             System.out.print("enter a guess: ");
-             guess = scanner.nextInt();
-             attempts++;
+//              System.out.print("enter a guess: ");
+//              guess = scanner.nextInt();
+//              attempts++;
 
-             if (guess < randomNum) {
-                 System.out.println("too low ");
-             } else if (guess > randomNum) {
-                 System.out.println("too high");
-             } else {
-                 System.out.println("CORRECT ! the number was " + randomNum);
-             }
+//              if (guess < randomNum) {
+//                  System.out.println("too low ");
+//              } else if (guess > randomNum) {
+//                  System.out.println("too high");
+//              } else {
+//                  System.out.println("CORRECT ! the number was " + randomNum);
+//              }
 
-         } while( guess != randomNum);
+//          } while( guess != randomNum);
 
-         System.out.println("You have won");
-         scanner.close();
-     }
- };
+//          System.out.println("You have won");
+//          scanner.close();
+//      }
+//  };
