9cc9e500bbad5660085551bd56bdc438a32eeeb0
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-02 18:33:47 +0530

    written code for guessing number

--- Class.java
+++ Class.java
@@ -1,5 +1,33 @@
- public class Class {
-     public static void main(String[] args) {
-        System.out.println();
+ import java.util.Random;
+ import java.util.Scanner;
+public class Class {
+     public static void main(String[] args){
+         Random random = new Random();
+         Scanner scanner = new Scanner(System.in);
+
+         int guess;
+         int attempts = 0;
+         int randomNum = random.nextInt(1 ,11);
+         System.out.println("NUmber guessing game");
+         System.out.println("Guess the number between 1-10: ");
+
+         do{
+             
+             System.out.print("enter a guess: ");
+             guess = scanner.nextInt();
+             attempts++;
+
+             if (guess < randomNum) {
+                 System.out.println("too low ");
+             } else if (guess > randomNum) {
+                 System.out.println("too high");
+             } else {
+                 System.out.println("CORRECT ! the number was " + randomNum);
+             }
+
+         } while( guess != randomNum);
+
+         System.out.println("You have won");
+         scanner.close();
      }
- }
+ };
