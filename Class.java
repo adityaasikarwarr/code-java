64f51993b36eb2dc6ee1db6892ea60d51700544b
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-02 18:13:12 +0530

    changes

--- Class.java
+++ Class.java
@@ -1,5 +1,5 @@
-//  public class Class {
-//      public static void main(String[] args) {
-//         System.out.println();
-//      }
-//  }
+ public class Class {
+     public static void main(String[] args) {
+        System.out.println();
+     }
+ }
