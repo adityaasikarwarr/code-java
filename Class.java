a9661e8755ce79b8cb17c2c4f1f2707ff91fb311
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-03 11:06:14 +0530

    changes

--- Class.java
+++ Class.java
@@ -34,32 +34,32 @@
 
  // a game
 
-public class Class {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+// public class Class {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
 
-        int rows;
-        int columns;
-        char symbol;
+//         int rows;
+//         int columns;
+//         char symbol;
 
-        System.out.print("Enter the number of rows: ");
-        rows = scanner.nextInt();
+//         System.out.print("Enter the number of rows: ");
+//         rows = scanner.nextInt();
 
-        System.out.print("Enter the number of columns: ");
-        columns = scanner.nextInt();
+//         System.out.print("Enter the number of columns: ");
+//         columns = scanner.nextInt();
 
-        System.out.print("Enter your symbol you want to use: ");
-        symbol = scanner.next().charAt(0);
+//         System.out.print("Enter your symbol you want to use: ");
+//         symbol = scanner.next().charAt(0);
 
-        for (int i = 0; i < rows  ; i++) {
-            for (int j = 0; j < columns  ; j++) {
-                System.out.print(symbol);
-        }
-            System.out.println();
-        }
-        scanner.close();
+//         for (int i = 0; i < rows  ; i++) {
+//             for (int j = 0; j < columns  ; j++) {
+//                 System.out.print(symbol);
+//         }
+//             System.out.println();
+//         }
+//         scanner.close();
 
-    }
-}
+//     }
+// }
 
  
