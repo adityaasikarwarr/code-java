616ebfe2c3f8da320b8c32481832fd95610c5fa6
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-03 10:32:00 +0530

    chanegs

--- Class.java
+++ Class.java
@@ -1,33 +1,33 @@
-//  // number guessing game
-//  import java.util.Random;
-//  import java.util.Scanner;
-// //public class Class {
-// //     public static void main(String[] args){
-// //         Random random = new Random();
-// //         Scanner scanner = new Scanner(System.in);
-// //
-// //         int guess;
-// //         int attempts = 0;
-// //         int randomNum = random.nextInt(1 ,11);
-// //         System.out.println("NUmber guessing game");
-// //         System.out.println("Guess the number between 1-10: ");
-// //
-// //         do{
-// //             System.out.print("enter a guess: ");
-// //             guess = scanner.nextInt();
-// //             attempts++;
-// //
-// //             if (guess < randomNum) {
-// //                 System.out.println("too low ");
-// //             } else if (guess > randomNum) {
-// //                 System.out.println("too high");
-// //             } else {
-// //                 System.out.println("CORRECT ! the number was " + randomNum);
-// //             }
-// //
-// //         } while( guess != randomNum);
-// //         System.out.println("your number of attempts are: " + attempts);
-// //         System.out.println("You have won");
-// //         scanner.close();
-// //     }
-// // };
+ // number guessing game
+ import java.util.Random;
+ import java.util.Scanner;
+//public class Class {
+//     public static void main(String[] args){
+//         Random random = new Random();
+//         Scanner scanner = new Scanner(System.in);
+//
+//         int guess;
+//         int attempts = 0;
+//         int randomNum = random.nextInt(1 ,11);
+//         System.out.println("NUmber guessing game");
+//         System.out.println("Guess the number between 1-10: ");
+//
+//         do{
+//             System.out.print("enter a guess: ");
+//             guess = scanner.nextInt();
+//             attempts++;
+//
+//             if (guess < randomNum) {
+//                 System.out.println("too low ");
+//             } else if (guess > randomNum) {
+//                 System.out.println("too high");
+//             } else {
+//                 System.out.println("CORRECT ! the number was " + randomNum);
+//             }
+//
+//         } while( guess != randomNum);
+//         System.out.println("your number of attempts are: " + attempts);
+//         System.out.println("You have won");
+//         scanner.close();
+//     }
+// };
