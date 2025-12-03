dcdc9ede457918c591aabb6e01779d048e5bc561
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-03 11:06:04 +0530

    wrote a code

--- Class.java
+++ Class.java
@@ -30,6 +30,36 @@
 //         System.out.println("You have won");
 //         scanner.close();
 //     }
-// };
+// };z
+
+ // a game
+
+public class Class {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter your symbol you want to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for (int i = 0; i < rows  ; i++) {
+            for (int j = 0; j < columns  ; j++) {
+                System.out.print(symbol);
+        }
+            System.out.println();
+        }
+        scanner.close();
+
+    }
+}
 
  
