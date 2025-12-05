fe1a03cd372d0308411aa64001ae23cdbfb20a09
adityasikarwarr <adityasikarwarr156@gmail.com>
2025-12-05 17:07:09 +0530

    changes in code

--- Hello.java
+++ Hello.java
@@ -1,62 +1,62 @@
- number guessing game
- public class Hello {
-    public static void main(String[] args){
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+//  number guessing game
+//  public class Hello {
+//     public static void main(String[] args){
+//         Random random = new Random();
+//         Scanner scanner = new Scanner(System.in);
 
-        int guess;
-        int attempts = 0;
-        int randomNum = random.nextInt(1 ,11);
-        System.out.println("NUmber guessing game");
-        System.out.println("Guess the number between 1-10: ");
+//         int guess;
+//         int attempts = 0;
+//         int randomNum = random.nextInt(1 ,11);
+//         System.out.println("NUmber guessing game");
+//         System.out.println("Guess the number between 1-10: ");
 
-        do{
-            System.out.print("enter a guess: ");
-            guess = scanner.nextInt();
-            attempts++;
+//         do{
+//             System.out.print("enter a guess: ");
+//             guess = scanner.nextInt();
+//             attempts++;
 
-            if (guess < randomNum) {
-                System.out.println("too low ");
-            } else if (guess > randomNum) {
-                System.out.println("too high");
-            } else {
-                System.out.println("CORRECT ! the number was " + randomNum);
-            }
+//             if (guess < randomNum) {
+//                 System.out.println("too low ");
+//             } else if (guess > randomNum) {
+//                 System.out.println("too high");
+//             } else {
+//                 System.out.println("CORRECT ! the number was " + randomNum);
+//             }
 
-        } while( guess != randomNum);
-        System.out.println("your number of attempts are: " + attempts);
-        System.out.println("You have won");
-        scanner.close();
-    }
-};z
+//         } while( guess != randomNum);
+//         System.out.println("your number of attempts are: " + attempts);
+//         System.out.println("You have won");
+//         scanner.close();
+//     }
+// };z
 
-public class Hello {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+// public class Hello {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
 
-        int rows;
-        int columns;
-        char symbol;
+//         int rows;
+//         int columns;
+//         char symbol;
 
-        System.out.print("Enter the number of rows: ");
-        rows = scanner.nextInt();
+//         System.out.print("Enter the number of rows: ");
+//         rows = scanner.nextInt();
 
-        System.out.print("Enter the number of columns: ");
-        columns = scanner.nextInt();
+//         System.out.print("Enter the number of columns: ");
+//         columns = scanner.nextInt();
 
-        System.out.print("Enter your symbol you want to use: ");
-        symbol = scanner.next().charAt(0);
+//         System.out.print("Enter your symbol you want to use: ");
+//         symbol = scanner.next().charAt(0);
 
-        for (int i = 0; i < rows  ; i++) {
-            for (int j = 0; j < columns  ; j++) {
-                System.out.print(symbol);
-        }
-            System.out.println();
-        }
-        scanner.close();
+//         for (int i = 0; i < rows  ; i++) {
+//             for (int j = 0; j < columns  ; j++) {
+//                 System.out.print(symbol);
+//         }
+//             System.out.println();
+//         }
+//         scanner.close();
 
-    }
-}
+//     }
+// }
 
  method OR function
  public class Hello {
