2daaa27778c27f66a713b9a47a336c4ecc8e558a
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-20 21:55:12 +0530

    changes

--- New.java
+++ New.java
@@ -1,23 +1,23 @@
 //     public static void main(String[] args) {
 //         Scanner input = new Scanner(System.in);
 
-//         // getting employee details
+        // getting employee details
 //         System.out.print("enter your name : ");
 //         String name = input.next();
 //         input.next();
-//         // System.out.println("your name is : " + name);
+        // System.out.println("your name is : " + name);
 
 //         System.out.print("Enter your id : ");
 //         int id = input.nextInt();
-//         // System.out.println("your id is : " + id);
+        // System.out.println("your id is : " + id);
 
 //         System.out.print("enter your age : ");
 //         int age = input.nextInt();
-//         // System.out.println("your age is : " + age);
+        // System.out.println("your age is : " + age);
 
 //         System.out.print("enter your salary : ");
 //         float salary = input.nextFloat();
-//         // System.out.println("your salry is : " + salary);
+        // System.out.println("your salry is : " + salary);
 
 //         System.out.println("Your name is " + name);
 //         System.out.println("Your age is " + age);
