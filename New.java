bca55be815185ec3b1c471e19cd4d8d59dff0ed5
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-21 20:59:41 +0530

    changes

--- New.java
+++ New.java
@@ -1,48 +1,48 @@
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        // getting employee details
-//         System.out.print("enter your name : ");
-//         String name = input.next();
-//         input.next();
-        // System.out.println("your name is : " + name);
+        getting employee details
+        System.out.print("enter your name : ");
+        String name = input.next();
+        input.next();
+        System.out.println("your name is : " + name);
 
-//         System.out.print("Enter your id : ");
-//         int id = input.nextInt();
-        // System.out.println("your id is : " + id);
+        System.out.print("Enter your id : ");
+        int id = input.nextInt();
+        System.out.println("your id is : " + id);
 
-//         System.out.print("enter your age : ");
-//         int age = input.nextInt();
-        // System.out.println("your age is : " + age);
+        System.out.print("enter your age : ");
+        int age = input.nextInt();
+        System.out.println("your age is : " + age);
 
-//         System.out.print("enter your salary : ");
-//         float salary = input.nextFloat();
-        // System.out.println("your salry is : " + salary);
+        System.out.print("enter your salary : ");
+        float salary = input.nextFloat();
+        System.out.println("your salry is : " + salary);
 
-//         System.out.println("Your name is " + name);
-//         System.out.println("Your age is " + age);
-//         System.out.println("Your salary is " + salary);
-//         System.out.println("YOur id is : " + id);
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+        System.out.println("Your salary is " + salary);
+        System.out.println("YOur id is : " + id);
 
-//         input.close();
+        input.close();
 
-//     }
-// public class New {
-//     public static void main(String[] args) {
-//         int[] numbers = {2 , 4 ,5 ,3 ,4 ,3 ,4 ,3};
-//         int sum = 0;
-//         Double average;
+    }
+public class New {
+    public static void main(String[] args) {
+        int[] numbers = {2 , 4 ,5 ,3 ,4 ,3 ,4 ,3};
+        int sum = 0;
+        Double average;
 
-//         for (int number : numbers){
-//             sum += number;
-//         }
+        for (int number : numbers){
+            sum += number;
+        }
 
-//         int arrayLength = numbers.length;
+        int arrayLength = numbers.length;
 
-//         average = ((double)sum / (double)arrayLength);
+        average = ((double)sum / (double)arrayLength);
 
-//         System.out.println("Sum = " + sum);
-//         System.out.println("Average = " + average);
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
 
-//     }
-// }
+    }
+}
