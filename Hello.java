11ffc04d14cf0e6bf2738ef10fb7ee666776a5a2
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-24 18:33:40 +0530

    change made a new file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
+
+import java.util.Scanner;
+
 public class Hello {
+ 
     public static void main(String[] args) {
+           Scanner scanner = new Scanner(System.in);
         System.err.println("hello world");
+        int num1 = scanner.nextInt();
+
+        System.err.println(num1);
+
+        scanner.close();
     }
 }
