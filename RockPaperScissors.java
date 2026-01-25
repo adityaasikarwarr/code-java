2f80e09d8a47756bcdb6e7162d9c6c580347e500
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-25 16:11:42 +0530

    made a new file and made changes

---
+++ RockPaperScissors.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = { "rock", "paper", "scissors" };
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        System.out.print("Enter your move (rock , paper , scissors): ");
+        playerChoice = scanner.nextLine().toLowerCase();
+
+        if (!playerChoice.equals("rock") &&
+                !playerChoice.equals("paper") &&
+                !playerChoice.equals("scissors")) {
+            System.out.println("Invalid Choice");
+        }
+
+        computerChoice = choices[random.nextInt(3)];
+        System.out.println("Computer Choice: " + computerChoice);
+
+        if (playerChoice.equals(computerChoice)) {
+
+            System.out.println("It's a tie!");
+
+        } else if (playerChoice.equals("rock") && computerChoice.equals("scissors")) {
+
+            System.out.println("You win!");
+
+        } else if (playerChoice.equals("paper") && computerChoice.equals("rock")) {
+
+            System.out.println("You win!");
+
+        } else if (playerChoice.equals("scissors") && computerChoice.equals("paper")) {
+
+            System.out.println("You win!");
+
+        } else {
+
+            System.out.println("You lose!");
+        }
+
+        scanner.close();
+        
+    }
+};
