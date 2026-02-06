b94b5e6989e014b653cff9830695139ef35a32e6
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-06 14:25:45 +0530

    changes

--- New.java
+++ New.java
@@ -1,15 +1,28 @@
 import java.util.*;
 
+// class New {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int n = scan.nextInt();
+//         int[] arr = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 0, };
+//         String text = "Hello World" ;
+//         for (int i = 0; i < arr.length; i++) {
+//             if (i % 2 == 0) {
+//                 System.out.println(arr[i]);
+//             }
+//         }
+//     }
+// }
+
 class New {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            System.out.println(i * i + " ");
-            sum = sum + (i * i);
-            System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        char[] str = text.toCharArray();
+        for (int i = 0; i < str.length; i++) {
+            if (!(str[i] >= 48 && str[i] <= 57)) {
+                System.out.println(str[i]);
+            }
         }
-        scan.close();
     }
 }
