890f9e0504adc91949673e9c2531f7ed21c269c0
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-06 14:53:44 +0530

    changes

--- New.java
+++ New.java
@@ -14,14 +14,39 @@ import java.util.*;
 //     }
 // }
 
+// class New {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String text = sc.nextLine();
+//         char[] str = text.toCharArray();
+//         for (int i = 0; i < str.length; i++) {
+//             if (!(str[i] >= 48 && str[i] <= 57)) {
+//                 System.out.println(str[i]);
+//             }
+//         }
+//     }
+// }
+
+// class New {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String str = sc.nextLine();
+//         String text = "";
+//         for (int i = 0; i <= str.length()/2; i++) {
+//             text = text + str.charAt(i) + "#";
+//             System.out.println(text);
+//         }
+//     }
+// }
 class New {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        char[] str = text.toCharArray();
-        for (int i = 0; i < str.length; i++) {
-            if (!(str[i] >= 48 && str[i] <= 57)) {
-                System.out.println(str[i]);
+        String str = sc.nextLine();
+        String text = "";
+        for (int i = 0; i <= str.length() / 2; i++) {
+            if (i % 2 == 0) {
+                text = text + str.charAt(i) + "#";
+                System.out.println(text);
             }
         }
     }
