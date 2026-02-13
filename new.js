a66c795cded0d5a5054e448fa2c2943e999a1f0c
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-13 22:51:31 +0530

    changes

--- new.js
+++ new.js
@@ -6,3 +6,17 @@
 // } else if (age >= 20) {
 //   console.log("adult");
 // }
+
+// let marks = 55;
+// if (marks >= 90) {
+//   console.log("a");
+// } else if (marks >= 70 && marks <= 89) {
+//   console.log("b");
+// } else if (marks >= 50 || marks <= 69) {
+//   console.log("c");
+// } else {
+//   console.log("fail");
+// }
+
+console.log(0 == false);
+console.log(0 === false);
