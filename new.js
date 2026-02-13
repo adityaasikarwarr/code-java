90e79d1d3e45911036e27b79f8de4ac11d06dcc2
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-13 22:45:52 +0530

    changes in codee

--- new.js
+++ new.js
@@ -1,8 +1,8 @@
-let hello = "name";
-hello = "hello";
-console.log(hello);
-console.log(5 == "5");
-let age = "20"
-if   (age == 20) {
-    console.log("hello")
-}
+// let age = 19;
+// if (age <= 13) {
+//   console.log("child");
+// } else if (age >= 18 && age <= 19) {
+//   console.log("Adult");
+// } else if (age >= 20) {
+//   console.log("adult");
+// }
