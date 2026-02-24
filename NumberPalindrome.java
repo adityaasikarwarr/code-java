a3940ef0250e3e4c3798dea5f2a5711ad60110f3
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-24 17:05:55 +0530

    changes in code

---
+++ NumberPalindrome.java
@@ -0,0 +1,24 @@
+public class NumberPalindrome {
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        int number = 121;
+
+        if (isPalindrome(number)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
--- new.js
+++ new.js
@@ -421,25 +421,25 @@
 //   .then((data) => console.log(data.id))
 //   .catch((error) => console.log(error));
 
-async function fetchData(params) {
-  try {
-    const pokemonName = document
-      .getElementById("pokemonName")
-      .value.toLowerCase();
+// async function fetchData(params) {
+//   try {
+//     const pokemonName = document
+//       .getElementById("pokemonName")
+//       .value.toLowerCase();
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+//     const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
 
-    if (!response.ok) {
-      throw new Error("not working");
-    }
+//     if (!response.ok) {
+//       throw new Error("not working");
+//     }
 
-    const data = await response.json();
-    const pokemonSprite = data.sprites.front_default;
-    const ImgElement = document.getElementById("pokemonSprite");
+//     const data = await response.json();
+//     const pokemonSprite = data.sprites.front_default;
+//     const ImgElement = document.getElementById("pokemonSprite");
 
-    ImgElement.src = pokemonSprite;
-    ImgElement.style.display = "block";
-  } catch (error) {}
-}
+//     ImgElement.src = pokemonSprite;
+//     ImgElement.style.display = "block";
+//   } catch (error) {}
+// }
 
-fetchData();
+// fetchData();
